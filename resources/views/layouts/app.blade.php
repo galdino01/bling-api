3386e3f57cdc3713e1373f41f49b4be6c6567f85
galdino01 <lucaspolucena@hotmail.com>
2022-07-20 19:56:24 -0300

    Updating active links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,20 +9,18 @@
     <title>@isset($metaTitle){{ config('app.name', 'Armazene Aqui') . ' | ' . $metaTitle }}@else{{ config('app.name', 'Armazene Aqui') }}@endisset</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
-        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap">
+
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 
     <!-- Powegrid and Livewire Styles -->
     @livewireStyles
@@ -116,13 +114,13 @@
     </main>
 
     <!-- JQuery Scripts -->
-    <script src="https://code.jquery.com/jquery-3.6.0.slim.js"
-        integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
 
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
+    <!-- AdminLTE Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 
     <!-- Powergrid and Livewire Scripts -->
     @livewireScripts
--- resources/views/layouts/supbar.blade.php
+++ resources/views/layouts/supbar.blade.php
@@ -1,16 +1,16 @@
 <ul class="nav supbar">
     <li class="nav-item">
-        <a class="nav-link @if (Route::getCurrentRoute()->getName() == 'dashboard') supbar-selected-link @endif" href="{{ route('dashboard') }}" title="Área da Dashboard">
+        <a class="nav-link @if (Request::is('dashboard')) supbar-selected-link @endif" href="{{ route('dashboard') }}" title="Área da Dashboard">
             <span class="text-light">Dashboard</span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link @if (Route::getCurrentRoute()->getName() == 'products.index') supbar-selected-link @endif" href="{{ route('products.index') }}" title="Área de Produtos">
+        <a class="nav-link @if (Request::is('products')) supbar-selected-link @endif" href="{{ route('products.index') }}" title="Área de Produtos">
             <span class="text-light">Produtos</span>
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link @if (Route::getCurrentRoute()->getName() == 'orders.index') supbar-selected-link @endif" href="{{ route('orders.index') }}" title="Área de Pedidos">
+        <a class="nav-link @if (Request::is('orders')) supbar-selected-link @endif" href="{{ route('orders.index') }}" title="Área de Pedidos">
             <span class="text-light">Pedidos</span>
         </a>
     </li>
